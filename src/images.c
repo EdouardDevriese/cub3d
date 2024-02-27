be789c5b57995a27510af16f6c9206e9a8fc7ce4
Victor Denissen <vdenisse@student.s19.be>
2024-02-27 15:49:20 +0100

    small fixes

--- src/images.c
+++ src/images.c
@@ -2,14 +2,13 @@
 
 int  get_image(t_drawing *d, char *path, t_orientation dir, void *mlx)
 {
-
-
-
 	d->tex[dir] = mlx_xpm_file_to_image(mlx, path, &d->texX, &d->texY);
 	if (!d->tex[dir])
 		printf("Error creating image [%s], in %s\n", path, __func__);
 	return (0);
 }
+
+//TODO create default  value should texture creation fail for any reason
 
 int32_t	rgb_to_int(int r, int g, int b)
 {
@@ -27,6 +26,7 @@ int  get_colour(int32_t *colour, char *rgb)
 	*colour = rgb_int;
 	return (0);
 }
+//TODO free split result && check for succes
 
 int get_draw_info_line(char *line, t_drawing *d, void *mlx)
 {
@@ -35,19 +35,32 @@ int get_draw_info_line(char *line, t_drawing *d, void *mlx)
 	path = ft_strtrim(path, " \n");
 	if (line[0] == 'N' && get_image(d, path, NO, mlx))
 		return (1);
-	if (line[0] == 'S' && get_image(d, path, SO, mlx))
+	else if (line[0] == 'S' && get_image(d, path, SO, mlx))
 		return (1);
-	if (line[0] == 'W' && get_image(d, path, WE, mlx))
+	else if (line[0] == 'W' && get_image(d, path, WE, mlx))
 		return (1);
-	if (line[0] == 'E' && get_image(d, path, EA, mlx))
+	else if (line[0] == 'E' && get_image(d, path, EA, mlx))
 		return (1);
-	if (line[0] == 'F' && get_colour(&d->ceiling, path))
+	else if (line[0] == 'F' && get_colour(&d->ceiling, path))
 		return (1);
-	if (line[0] == 'C' && get_colour(&d->ceiling, path))
+	else if (line[0] == 'C' && get_colour(&d->ceiling, path))
 		return (1);
 	return (0);
 }
 
+int	check_info(t_drawing *d)
+{
+	if (!d->tex[NO])
+		return (1);
+	if (!d->tex[SO])
+		return (1);
+	if (!d->tex[EA])
+		return (1);
+	if (!d->tex[WE])
+		return (1);
+	return (0);
+}
+//TODO how to check for failure of getting the colour values
 
 int get_draw_info(t_drawing *d, void *mlx)
 {
@@ -56,9 +69,8 @@ int get_draw_info(t_drawing *d, void *mlx)
 
 	while (new_line)
 	{
-		if (is_map(new_line))
-			break ;
-		get_draw_info_line(new_line, d, mlx);	
+		if (!is_map(new_line) && *new_line != '\n')
+			get_draw_info_line(new_line, d, mlx);	
 		free(new_line);
 		new_line = get_next_line(fd);
 	}
